c90f9494c0e46f0ad011a48edba17e0571e31370
Atigorn.S <ati2rok@gmail.com>
2026-02-02 15:02:02 +0700

    version 1.1 ิ่ม tri_cal()

--- i.py
+++ i.py
@@ -1,6 +1,8 @@
 def area_cal(x,y):
     reg_cal = x * y
-    return (reg_cal)
+    tri_cal = (1/2) * x * y
+    return (reg_cal, tri_cal)
 
-reg = area_cal(4,2)
-print(reg)
+reg, tri = area_cal(5,3)
+print(f'พื้นที่คำนวนสี่เหลี่ยม  : {reg}')
+print(f'พื้นที่คำนวนสามเหลี่ยม : {tri}')
